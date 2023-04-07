f0d71ec8a4035a40d9756cc2f7511c8e920defef
Mateus Araújo <smaithorz@gmail.com>
2023-04-07 15:29:04 -0300

    Few more concepts

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal class Program
+    {
+        enum Cor
+        {
+            Vermelho, //0
+            Amarelo, //1
+            Verde, //2
+            Roxo //3
+        }
+
+        enum Carros
+        {
+            Saveiro,
+            Ferrari,
+            Gol,
+            Toyota
+        }
+        static void Main(string[] args)
+        {
+
+            Cor minhaCor = Cor.Roxo;
+            Carros meuCarro = Carros.Toyota;
+
+            Console.WriteLine(meuCarro + " "+ minhaCor);
+
+           
+
+           
+
+            
+        }
+
+       
+    }
+}
---
+++ GettingUserInput/GettingUserInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digit a number:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Digite a second number");
+            string input2 = Console.ReadLine();
+
+            int input1Converter = int.Parse(input1);
+            int input2Converter = int.Parse(input2);
+
+            int sum = input1Converter + input2Converter;
+
+            Console.WriteLine("The result is: " + sum);
+        }
+    }
+}
